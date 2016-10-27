1e6296d20226e1d08236a2a4f8f5daef62b88838
Dastan Akhmetov <ddakhmet@gmail.com>
2016-10-27 20:35:35 +0600

    Createad a migration for news articles table

---
+++ database/migrations/2016_10_27_143249_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Creates a table of news articles
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('body');
+            $table->string('photo_url');
+            $table->integer('author_id');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Drops a table of news articles
+        Schema::dropIfExists('articles');
+    }
+}
